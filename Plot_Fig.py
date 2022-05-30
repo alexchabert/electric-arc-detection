4f627b15e6d13d302c8326230e9130c32f8458be
alexchabert <87310147+alexchabert@users.noreply.github.com>
2022-05-30 10:13:30 +0200

    Update Plot_Fig.py

--- Plot_Fig.py
+++ Plot_Fig.py
@@ -7,13 +7,19 @@ Created on Fri Apr 22 11:18:36 2022
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#load panda dataframe
 df=pd.read_csv('prediction_article_transformer.csv')  
 
+#load columns 'time','target','I' and 'prediction'
 t=df['time']
 target=df['target']
 current=df['I']
 pred=df['prediction']
 
-plt.plot(current)
-plt.plot(pred)
-plt.plot(target)
+#Plot
+#Warning: each array contains around 1 million points
+#slicing might be required to reduce memory consumption
+#(e.g. plt.plot(t[:50000],current[:50000]))
+plt.plot(t,current)
+plt.plot(t,pred)
+plt.plot(t,target)
